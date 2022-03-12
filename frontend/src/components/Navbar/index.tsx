84606075358e80d173de70ecc3053795b1edc7a0
Matheus Lannes <matheuslannes99@gmail.com>
2022-03-12 16:12:02 -0300

    First request

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/matheuslannes"> 
                     <div className='dsmovie-contact-container'>
                         <GithubIcon />
-                        <p className='dsmovie-contact-link'> /devsuperior</p>
+                        <p className='dsmovie-contact-link'> /matheuslannes</p>
                 </div>
                     </a>
                  </div>         
